18ff60d76f48265db091a7d413e182a19fb46b2e
Emik <emik.lakunishok@gmail.com>
2024-08-31 19:00:04 +0200

    Add API for those who directly reference this source generator

--- Source/OperatorGenerator.cs
+++ Source/OperatorGenerator.cs
@@ -8,19 +8,45 @@ public sealed class OperatorGenerator : IIncrementalGenerator
     /// <inheritdoc />
     void IIncrementalGenerator.Initialize(IncrementalGeneratorInitializationContext context)
     {
-        var isDefined = context.CompilationProvider.Select(IsValueTupleDefined);
-
         var provider = context
            .SyntaxProvider
            .AgainstAttributeWithMetadataName(Of<AttributeGenerator>(), Is<BaseTypeDeclarationSyntax>, Target)
            .Filter()
-           .Combine(isDefined)
+           .Combine(context.CompilationProvider.Select(IsValueTupleDefined))
            .Select(TryAddSource)
            .Filter()
            .Select(AddHintName);
 
         context.RegisterSourceOutput(provider, AddSource);
     }
+
+    /// <summary>Executes this source generation based on given input.</summary>
+    /// <remarks><para>
+    /// This API is unused within the library, but can be used by other libraries that reference
+    /// this library to integrate this source generator however they wish. Do note that the API
+    /// is subject to change, but the general premise of the function will remain the same.
+    /// </para></remarks>
+    /// <param name="named">The type to generate for.</param>
+    /// <param name="hasValueTuple">
+    /// If the compilation supports the <see cref="ValueTuple"/> type. Set this to <see langword="false"/> if an older
+    /// framework is used without a polyfill of said type. Such older frameworks include <c>.NET Framework 4.6.2</c>
+    /// or earlier, <c>.NET Standard 1.6</c> or earlier, <c>.NET Core 1.0</c>, and <c>.NET Core 1.1</c>.
+    /// </param>
+    /// <returns>
+    /// The generated source. This contains the hint name, which indicates the name of the file, and the source,
+    /// representing the contents of said file. This function will return <see langword="null"/> under a few
+    /// circumstances. This includes the parameter <paramref name="named"/> being <see langword="null"/>, being a tuple
+    /// type regardless of if it is a polyfill or not, not having any suitable constructors to wrap implicit operators
+    /// around, not being fully <see langword="partial"/> which includes any types that contain the parameter
+    /// <paramref name="named"/>.
+    /// </returns>
+    [Pure]
+    public static GeneratedSource? Transform(INamedTypeSymbol? named, bool hasValueTuple = true) =>
+        named is { IsTupleType: false } &&
+        named.IsCandidate() &&
+        TryAddSource((named, hasValueTuple), default) is { } tuple
+            ? AddHintName(tuple, default)
+            : null;
 
     static bool IsValueTupleDefined(Compilation compilation, CancellationToken token) =>
         compilation.GetTypesByMetadataName($"{nameof(System)}.{nameof(ValueTuple)}").Any();
